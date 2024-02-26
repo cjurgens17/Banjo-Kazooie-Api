6b5c1e704f15ef3f92ccac78c4b77f8543c40d4c
chris <104324755+cjurgens17@users.noreply.github.com>
2024-02-25 19:03:36 -0500

    commiting initial project

---
+++ src/main/java/com/banjo/bkapi/BkapiApplication.java
@@ -0,0 +1,13 @@
+package com.banjo.bkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banjo/bkapi/BkapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banjo.bkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
